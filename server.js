e0ac1aa1f74def77f0d8f04cdbcfe8959364168d
zerohoney <88021388+zerohoney@users.noreply.github.com>
2022-08-10 21:38:23 +0900

    completed http cors issue

--- server.js
+++ server.js
@@ -1,25 +1,413 @@
 const express = require('express');
+const path = require('path');
 const app = express();
+const http=require('http');
+const https=require('https');
+const fs=require('fs');
+const MongoClient = require('mongodb').MongoClient;
+const bcrypt=require('bcrypt');
+const  axios = require('axios');
+const cors=require('cors');
+const { ObjectId } = require('mongodb');
+require('dotenv').config();
 
-app.listen(8080, function() {
-    console.log('listening on 8080')
+let bookFloorList, computerProgramList, daisyList, digitalBookList, hanofficeList, internetList, ocrList, screenReaderList, smartPhoneList;
+let base,engBraille,hangeul,symbol,touch;
+let connetToZeroHoneyMongoDb = 'mongodb+srv://zerohoney:e911291!e9@cluster0.wlnrf.mongodb.net/?retryWrites=true&w=majority';
+
+MongoClient.connect(connetToZeroHoneyMongoDb,function(err,client){
+
+//  "proxy": "http://localhost:8080"
+
+// node.js
+// 전역변수
+var dbAccount=client.db('Account');
+var dbStudent=client.db('Student');
+var dbEccList=client.db('EccList');
+// 각 폼을 불러와주는 쿼리값
+// 리스트를 임시보관할 array
+var tempList;
+
+
+
+// 컬렉션 이름 바꾸는 메소드
+// MongoClient.connect('mongodb+srv://ecoco97:e911291!e9@eccproject.7wg6l.mongodb.net/EccSubtech?retryWrites=true&w=majority',function (err,client){
+//     db=client.db('EccSubtech');
+//     db.collection('form_daisy').rename('form/0/2');
+
+//     })
+
+app.use(express.urlencoded({ extended: true }))
+// var whitelist = ['*']
+// var corsOptions = {  origin: function(origin, callback){  var isWhitelisted = whitelist.indexOf(origin) !== -1; 
+//    callback(null, isWhitelisted);  
+//   //   callback expects two parameters: error and options  
+//    },  credentials:true}
+
+// app.use(cors(corsOptions),()=>{console.log('미들웨어 성공')});
+http.createServer((req,res)=>{
+  res.setHeader("Access-Control-Allow-Origin", "*");
+})
+app.listen(8080,(req,res)=>{
+  
+    console.log('성공했구나 이녀석..')
+})
+
+// var server=http.createServer(app).listen(8080,function(req,res){
+  
+//     console.log('또 성공했구나..이녀석');
+// })
+
+
+// https.createServer(options,app).listen(443,function(){
+//     console.log('또 또 성공했구나..이녀석');
+// })
+
+// 1.백엔드 2.프론트엔드 3.미들웨어
+
+
+
+
+app.use(express.static(path.join(__dirname,'public')))
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname+ '/index.html'))
 })
 app.get('/', function(요청, 응답) { 
-    응답.sendFile(__dirname +'/index.html')
-  });
+  응답.sendFile(__dirname +'/index.html')
+});
+
 app.get('/pet', function(요청, 응답) { 
     응답.send('펫용품 사시오')
   })
 
-  app.get('/user',function(req,res){
-    res.header("Access-Control-Allow-Origin", "*");
-    var tempUser={"name":"asd",
-    "birth":970204,
-    "email":"으헤헤헤이거슨이메일",
-    "uid":"유니크다음은 전설"
+app.get('/test', (req, res) => {
+    MongoClient.connect('mongodb+srv://ecoco97:e911291!e9@eccproject.7wg6l.mongodb.net/test?retryWrites=true&w=majority', function (err, client) {
+        db = client.db('test');
+        db.collection('test_apple').find().toArray((err, result) => { res.send(result) });;
+
+    })
+})
+// google-site-verification=uR1kEOUbJS60DOSd7pJM70Gpx8YqKfY-jjPk1oY_QUk
+// 파이어베이스 테스트용
+app.post('/testFire',(req,res)=>{
+database.ref('ECC').once('보조공학').then(function(snapshot){
+    console.log(snapshot.val());
+})
+
+})
+
+app.get('/getEccList', (req, res) => {
+  res.header("Access-Control-Allow-Origin", "*");
+ 
+   var EccList=  dbEccList.collection('SubTech');
+    const {data}=req.query;
+    const dataArray=data.split('/');
+    console.log(dataArray[1]);
+    if(parseInt(dataArray[0])===0){
+EccList.find({_id:ObjectId('62cbff16c405696e9086655c')}).toArray((err,res)=>{
+    switch(parseInt(dataArray[1])){
+        case 0:console.log(res[0].보조공학.책마루);
+        break;
+        case 1:console.log(res[0].보조공학.OCR);  
+        break;     
+        case 2:console.log(res[0].보조공학.데이지플레이어);
+        break;
+        case 4:console.log(res[0].보조공학.스마트기기활용);
+        break;
+        case 5:console.log(res[0].보조공학.스크린리더);
+        break;
+        case 6:console.log(res[0].보조공학.인터넷활용);
+        break;
+        case 7:console.log(res[0].보조공학.전자교재);
+        break;
+        case 8:console.log(res[0].보조공학.컴퓨터및프로그램);
+        break;
+        case 9:console.log(res[0].보조공학.한글및오피스);
+        break;
+
+
+
     }
+})
+
+    } 
+
+
+})
+
+app.get('/getOcr', (req, res) => {
+    MongoClient.connect('mongodb+srv://ecoco97:e911291!e9@eccproject.7wg6l.mongodb.net/EccSubtech?retryWrites=true&w=majority', function (err, client) {
+
+        db = client.db('EccSubtech');
+        db.collection('form_ocr').find().toArray((err, result) => { console.log(result); if (err) console.log(err); res.send(result) });
+
+    })
+
+})
+
+ app.get("/getSubtech", (req, res) => {
+    //ecc보조공학을 임시 보관하는 array
+    MongoClient.connect(connetToZeroHoneyMongoDb,function(err,client){
+        db=client.db('EccSubtech');
+        db.collection('list').find().toArray((err,result)=>{
+            if(err) throw err;
+          
+            bookFloorList = result[0].bookFloor;
+            computerProgramList = result[1].computerProgram;
+            daisyList = result[2].daisy;
+            digitalBookList = result[3].digitalBook;
+            hanofficeList = result[4].hanoffice;
+            internetList = result[5].internet;
+            ocrList = result[6].ocr;
+            screenReaderList = result[7].screenReader;
+            smartPhoneList = result[8].smartPhone;
+            tempList=[bookFloorList,computerProgramList,daisyList,digitalBookList,hanofficeList,internetList,ocrList,screenReaderList,smartPhoneList];
+
+            res.send(tempList);
+        });
+
+        })
+}
+
+)
+
+app.get('/getMoontype',(req,res)=>{
+    MongoClient.connect(connetToZeroHoneyMongoDb,function(err,client){
+
+db=client.db(EccMoonType);
+db.collection('list').find().toArray((err,result)=>{
+if(err) throw console.log(err);
+
+base=result[0].base;
+engBraille=result[1].engBraille;
+hangeul=result[2].hangeul;
+symbol=result[3].symbol;
+touch=result[4].touch;
+tempList=[base,engBraille,hangeul,symbol,touch];
+
+res.send(tempList);
+})
+
+    })
+
+
+
+})
+app.get('/getStudentListFromDB',function(req,res){
+  res.setHeader("Access-Control-Allow-Origin", "*");
+dbStudent.collection('A').find({}).toArray(function(err,result){
+    if (err) throw err;
+    console.log(result);
+res.send(result);
+})
+
+})
+app.get('/user',function(req,res){
+  res.setHeader("Access-Control-Allow-Origin", "*");
+var tempUser={"name":"asd",
+"birth":970204,
+"email":"으헤헤헤이거슨이메일",
+"uid":"유니크다음은 전설"
+}
+
+res.send(tempUser);
+console.log(tempUser);
+})
+
+
+
+
+
+
+
+// ECC REST API 통신
+app.get('*', function (req, res) {
+    res.sendFile(path.join(__dirname, 'build/index.html'))
+
+})
+
+
+
+
+
+// DB에서 학생정보 가져오는 메소드
+
+
+app.post('/sendData', (req, res) => {
+    data = req.body.data;
+})
+
+
+
+
+app.post('/addSome', (req, res) => {
+
+    MongoClient.connect('mongodb+srv://ecoco97:e911291!e9@eccproject.7wg6l.mongodb.net/test?retryWrites=true&w=majority', function (err, client) {
+        var para1 = req.body.log1;
+        var para2 = req.body.log2;
+
+        db = client.db('test')
+        db.collection('user').insertOne({ name: para1, password: para2 }, function (err, result) {
+            console.log('저장완료');
+        });
+
+
+        // db.collection('test_apple').ubdateOne({},{})
+    })
+
+})
+app.post('/enroll', (req, res) => {
+
+    const tem = req.body.data;
+
+    Add.addStudent(tem, res);
+
+    // db.collection('test_apple').ubdateOne({},{})
+})
+
+
+
+
+
+app.post('/addForm', (req, res) => {
+
+    MongoClient.connect('mongodb+srv://ecoco97:e911291!e9@eccproject.7wg6l.mongodb.net/EccSubtech?retryWrites=true&w=majority', function (err, client) {
+
+        db = client.db('EccSubtech')
+        db.collection('list').insertOne({ name: '하이' }, function (err, result) {
+            console.log('저장완료');
+        });
+
+
+        // db.collection('test_apple').ubdateOne({},{})
+    })
+
+})
+
+
+
+// ecc데이터 저장 메소드 시작
+app.post('/addScoreOcr', (req, res) => {
+    Post.addScoreSubtechOcr(req.body.btn);
+})
+app.post('/addScoreDaisy', (req, res) => {
+
+    Post.addScoreSubtechDaisy(req.body.btn);
+    // 만약 점수 2번 버튼을 누른다면 가져온 아이디를 검사하여 사전,사후,현재 평가에 2점 부여
+})
+
+// 회원가입 메소드
+app.post('/doingSignUp',(req,res)=>{
+  res.header("Access-Control-Allow-Origin", "*");
+
+        userInformation=req.body.user;
+        // split으로 쪼개서 각각 대입
+        let info=userInformation.split('___');
+        bcrypt.genSalt(10, function (err, salt) {
+            if (err) return err;
+            bcrypt.hash(info[3], salt, function (err, hashedPassword) {
+                if (err) return err;
+                info[3]=hashedPassword;
+                  console.log(info[0]);
+                   dbAccount.collection('User').insertOne({name:info[0],birth:info[1],email:info[2],password:info[3]}
+                    ,function(err,result){
+           })
+            })
+
+        }) 
+      res.redirect('/')
+
+})
+
+// 회원가입 실패시 새로고침 메소드
+
+app.post('/again',(req,res)=>{
+  res.header("Access-Control-Allow-Origin", "*");
+    console.log('성공');
+    res.send(`<script>
+    alert('회원 정보에 _가 포함되어 있습니다.');
+    history.back();
+</script>`);
+// redirect로 뒤로가기 하는법
+    // res.redirect(req.header('Referer')|| '/');
+})
+// ecc데이터 저장 메소드 끝
+
+// 회원 인증 API
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+const session = require('express-session');
+const { send } = require('process');
+const { throws } = require('assert');
+
+app.use(session({ secret: 'secretCode', resave: true, saveUninitialized: false }));
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.get('/login',function(req,res){
+
+
+})
+
+app.post('/login',passport.authenticate('local',{
     
-    res.send(tempUser);
-    console.log(tempUser);
-    });
+failureRedirect:'/signUp'
+
+}),function(req,res){
     
+res.redirect('/');
+})
+
+// 로그인 검사 미들웨어
+
+passport.use(new LocalStrategy({
+    // name 속성
+    usernameField: 'email',
+    passwordField: 'password',
+    session: true,
+    passReqToCallback: false,
+  }, function (email, password, done) {
+  
+    
+    dbAccount.collection('User').findOne({ email: email }, function (err, res) {
+      if (err) return done(err)
+  
+      if (!res) return done(null, false, { message: '존재하지않는 아이디요' })
+     bcrypt.compare(password,res.password).then((isMatch)=>{
+console.log(password, res.password);
+if(isMatch){
+return done(null,res);
+
+}else{return done(null.false,{message:'비번 틀렸어요'})}
+
+     });
+    })
+  }));
+
+  passport.serializeUser(function(res,done){
+done(null,res.email)
+
+  });
+passport.deserializeUser(function(id,done){
+done(null,{})
+
+});
+})
+
+//   원본 passport
+//   passport.use(new LocalStrategy(function verify(username, password, cb) {
+//     db.get('SELECT * FROM users WHERE username = ?', [ username ], function(err, row) {
+//       if (err) { return cb(err); }
+//       if (!row) { return cb(null, false, { message: 'Incorrect username or password.' }); }
+  
+//       crypto.pbkdf2(password, row.salt, 310000, 32, 'sha256', function(err, hashedPassword) {
+//         if (err) { return cb(err); }
+//         if (!crypto.timingSafeEqual(row.hashed_password, hashedPassword)) {
+//           return cb(null, false, { message: 'Incorrect username or password.' });
+//         }
+//         return cb(null, row);
+//       });
+//     });
+//   }));
